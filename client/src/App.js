8da6e6eb2ad9385fd3b6cce81f7eb155d890f331
Perry Miller <Perry.Miller@esi-group.com>
2021-04-02 19:58:43 -0400

    Added client and server folders

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App()
+{
+  const [ count, setCount ] = useState ( 0 );
+
+  useEffect ( () =>
+  {
+    document.title = `You clicked ${count} times`;
+  } );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload wow.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
